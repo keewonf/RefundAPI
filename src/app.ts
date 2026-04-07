5c7ef8d0a55f057f6b614daaae26c37aa3dbd886
keewonf <lukasfmm@gmail.com>
2026-04-07 19:59:58 -0300

    feat: add global error handling middleware with zod validation support

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import cors from "cors";
+import { errorHandling } from "./middlewares/error-handling";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 app.get("/", (req, res) => {
   return res.json({ message: "Ok" });
 });
+
+app.use(errorHandling)
 export { app };
