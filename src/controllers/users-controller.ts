6460140b265d4b9fffa049c896dca730f307b684
keewonf <lukasfmm@gmail.com>
2026-04-09 07:18:44 -0300

    feat(users): implement user creation with validation and password hashing

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,11 +1,46 @@
 import { Request, Response } from "express";
+import { UserRole } from "@/generated/prisma/client";
 import { z } from "zod";
+import { hash } from "bcrypt";
 import { AppError } from "@/utils/AppError";
 import { prisma } from "@/database/prisma";
 
+const bodySchema = z.object({
+  name: z
+    .string()
+    .trim()
+    .min(2, { message: "Name must be at least 2 characters long" }),
+  email: z.email({ message: "Invalid e-mail" }).trim(),
+  password: z.string().min(6, "Password must be at least 6 characters long"),
+  role: z
+    .enum([UserRole.employee, UserRole.manager])
+    .default(UserRole.employee),
+});
+
 class UsersController {
   async create(req: Request, res: Response) {
-    return res.status(201).json({ message: "ok" });
+    const { name, email, password, role } = bodySchema.parse(req.body);
+
+    const userWithSameEmail = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (userWithSameEmail) {
+      throw new AppError("User with same email already exists");
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        role,
+      },
+    });
+
+    return res.status(201).json({ message: "Created!" });
   }
 }
 
