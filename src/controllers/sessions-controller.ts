809a15c0f662e0556162fbb3c1af8652f507cba5
keewonf <lukasfmm@gmail.com>
2026-04-09 07:19:40 -0300

    feat(auth): add session login and route with JWT token generation

---
+++ src/controllers/sessions-controller.ts
@@ -0,0 +1,45 @@
+import { Request, Response } from "express";
+import { AppError } from "@/utils/AppError";
+import { authConfig } from "@/configs/auth";
+import { prisma } from "@/database/prisma";
+import { sign } from "jsonwebtoken";
+import { compare } from "bcrypt";
+import { z } from "zod";
+
+const bodySchema = z.object({
+  email: z.email({ message: "Invalid e-mail" }).trim(),
+  password: z.string(),
+});
+
+class SessionsController {
+  async create(req: Request, res: Response) {
+    const { email, password } = bodySchema.parse(req.body);
+
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new AppError("Invalid email or password", 401);
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new AppError("Invalid email or password", 401);
+    }
+
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({ role: user.role }, secret, {
+      subject: user.id,
+      expiresIn,
+    });
+
+    const { password: hashedPassword, ...userWithoutPassword } = user;
+
+    return res.json({ token, userWithoutPassword });
+  }
+}
+
+export { SessionsController };
