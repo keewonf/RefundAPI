19d3d52b67e4a68eb4b4f53c784aaa7898eb91a3
keewonf <lukasfmm@gmail.com>
2026-04-09 07:48:09 -0300

    fix(routes): instantiate SessionsController with constructor call

--- src/routes/sessions-routes.ts
+++ src/routes/sessions-routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { SessionsController } from "@/controllers/sessions-controller";
 
 const sessionsRoutes = Router()
-const sessionsController = new SessionsController
+const sessionsController = new SessionsController()
 
 sessionsRoutes.post("/", sessionsController.create)
 
