6882019cc8344d6f1adb0be1a45de3b8d403f4a6
keewonf <lukasfmm@gmail.com>
2026-05-07 16:47:22 -0300

    refactor: separate file upload from refund creation

--- src/routes/refunds-routes.ts
+++ src/routes/refunds-routes.ts
@@ -1,22 +1,18 @@
 import { Router } from "express";
-import multer from "multer";
 import { RefundsController } from "@/controllers/refunds-controller";
 import { verifyUserAuthorization } from "@/middlewares/verify-user-authorization";
-import uploadConfig from "@/configs/upload";
 
 const refundsRoutes = Router();
 const refundsController = new RefundsController();
-const upload = multer(uploadConfig.MULTER);
 
 refundsRoutes.post(
   "/",
-  verifyUserAuthorization(["manager"]),
-  upload.single("file"),
+  verifyUserAuthorization(["manager", "employee"]),
   refundsController.create,
 );
 refundsRoutes.get(
   "/",
-  verifyUserAuthorization(["manager"]),
+  verifyUserAuthorization(["manager", "employee"]),
   refundsController.index,
 );
 refundsRoutes.get(
